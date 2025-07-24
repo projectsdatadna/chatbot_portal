f295b1828110d0cb4b65a190defbf3a379159a8f
projectsdatadna <projects@datadna.in>
2025-07-24 18:37:55 +0530

    api changes

--- src/ApiService.ts
+++ src/ApiService.ts
@@ -3,36 +3,36 @@ import axios from "axios"
 const LOCALURL = import.meta.env.VITE_API_URL;
 
 
-// export function manualSearch(data:any){
-//     return axios.post(`${LOCALURL}/peopleSearch`,data)
-// }
+export function manualSearch(data:any){
+    return axios.post(`${LOCALURL}/peopleSearch`,data)
+}
 
 
-// export function statusAutomation(){
-//     return axios.get(`${LOCALURL}/statusAutomation`)
-// }
+export function statusAutomation(){
+    return axios.get(`${LOCALURL}/statusAutomation`)
+}
 
-// export function startAutomation(){
-//     return axios.post(`${LOCALURL}/startAutomation`)
-// }
-
-
-// export function stopAutomation(){
-//     return axios.post(`${LOCALURL}/stopAutomation`)
-// }
+export function startAutomation(){
+    return axios.post(`${LOCALURL}/startAutomation`)
+}
 
 
-// export function processDetail(){
-//     return axios.post(`${LOCALURL}/processDetail`)
-// }
+export function stopAutomation(){
+    return axios.post(`${LOCALURL}/stopAutomation`)
+}
 
-// export function getFileList(){
-//     return axios.get(`${LOCALURL}/filesList`)
-// }
 
-// export function downloadFile(name:string){
-//     return axios.get(`${LOCALURL}/downloadFile/${name}`)
-// }
+export function processDetail(){
+    return axios.post(`${LOCALURL}/processDetail`)
+}
+
+export function getFileList(){
+    return axios.get(`${LOCALURL}/filesList`)
+}
+
+export function downloadFile(name:string){
+    return axios.get(`${LOCALURL}/downloadFile/${name}`)
+}
 
 
 export function chat(data:any){
